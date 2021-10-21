017be34d6b56821af45cb758d670cee8a36253d2
Ricardo Plaza <code.rcplaza@gmail.com>
2021-10-20 23:17:14 -0300

    Add routing and test routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
 import { useState } from "react";
-import logo from "./assets/Logo_ML.png";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 const App = () => {
   const [text, setText] = useState("");
 
   return (
-    <div className="container">
-      <img src={logo} alt="MeLi" />
-      <label htmlFor="text">Texto: </label>
-      <input
-        id="text"
-        type="text"
-        value={text}
-        onChange={({ target }) => setText(target.value)}
-      />
-    </div>
+    <Router>
+      <header>
+        <h1>Mercado Libre</h1>
+        <Link to="/">Home</Link>
+        <Link to="/adad">About</Link>
+        <Link to="/users">Users</Link>
+      </header>
+      <main>
+        <Switch>
+          <Route exact path="/adad">
+            <About />
+          </Route>
+          <Route exact path="/users">
+            <Users />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/">404 - Not Found</Route>
+        </Switch>
+      </main>
+    </Router>
   );
 };
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
 
 export default App;
